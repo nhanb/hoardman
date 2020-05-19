eedbdf76ed2c69d36b1c67be47a7935cf96b5a11
Bùi Thành Nhân <hi@imnhan.com>
2020-05-19 14:38:08 +0700

    configurable proxy

--- httpclient.py
+++ httpclient.py
@@ -7,11 +7,24 @@ HEADERS = {
 }
 
 PASSWORD = os.environ["GCFPROXY_PASSWORD"]
+REGION_INDEX = int(os.environ["GCFPROXY_INDEX"])
+REGIONS = [
+    "asia-east2",
+    "asia-northeast1",
+    "europe-west1",
+    "europe-west2",
+    "us-central1",
+    "us-east1",
+    "us-east4",
+]
+
+PROXY = f"https://{REGIONS[REGION_INDEX]}-nhansproxy.cloudfunctions.net/nhansproxy"
+print(PROXY)
 
 
 def proxied_get(url, timeout=10):
     return requests.post(
-        "https://asia-east2-nhansproxy.cloudfunctions.net/nhansproxy",
+        PROXY,
         json={
             "url": url,
             "method": "get",
