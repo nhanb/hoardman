493bf7f7eab2bf30d1b52cea4cfafb6ee1db7017
Bùi Thành Nhân <hi@imnhan.com>
2020-05-19 12:04:00 +0700

    switch proxy

--- httpclient.py
+++ httpclient.py
@@ -6,12 +6,12 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
 }
 
-PASSWORD = os.environ["GAEPROXY_PASSWORD"]
+PASSWORD = os.environ["GCFPROXY_PASSWORD"]
 
 
 def proxied_get(url, timeout=10):
     return requests.post(
-        "https://nhansproxy.df.r.appspot.com/proxy",
+        "https://asia-east2-nhansproxy.cloudfunctions.net/nhansproxy",
         json={
             "url": url,
             "method": "get",
